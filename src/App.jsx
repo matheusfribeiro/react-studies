ea2f237dfc77db22eac9c8bf943477360f73ed5a
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-04-09 18:51:25 -0300

    ternary operator

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       {planets.map((planet, key) => {
-        if (planet.isGasPlanet) return <h1> {planet.name} </h1>
+        if (planet.isGasPlanet) return <h1 key={key}> {planet.name} </h1>
       })}
       
     </div>
