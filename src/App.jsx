c23d617b66435b5efb396f80330ed339ea3d37b2
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-04-16 18:18:48 -0300

    use context hook, state management

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
 import './App.css'
 import Axios from 'axios'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, createContext } from 'react'
 import {createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider} from 'react-router-dom' 
 import { Home } from './pages/Home'
 import { Menu } from './pages/Menu'
 import { Contact, dataLoader } from './pages/Contact'
-import { Navbar } from './NavBar'
+import { Profile } from './pages/Profile'
 
 
-
+export const AppContext = createContext()
 
 function App() {
+  const [username, setUsername] = useState("MattTech")
 
   const router = createBrowserRouter(
     createRoutesFromElements(
+      
       <Route path='/' element={<Root />}>
         <Route index element={<Home />} />
         <Route path='/contact' element={<Contact />} loader={dataLoader} />
         <Route path='/menu' element={<Menu />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<h1>PAGE NOT FOUND!!!</h1>} />
       </Route>
+      
     )
   )
 
@@ -26,7 +31,10 @@ function App() {
   return (
     <div className="App">
       <h1>Hello react</h1>
-      <RouterProvider router={router} />
+      <AppContext.Provider value={{username, setUsername}}>
+        <RouterProvider router={router} />
+      </AppContext.Provider>
+      
     </div>
   )
 }
@@ -38,6 +46,7 @@ const Root = () => {
         <Link to="/"> Home </Link>
         <Link to="/menu"> Menu </Link>
         <Link to="/contact"> Contact </Link>
+        <Link to="/profile"> Profile </Link>
         
       </div>
       <div>
@@ -47,7 +56,37 @@ const Root = () => {
   )
 }
 
-
-
-
 export default App
+
+const TopComponent = () => {
+  const state = 'test for echo'
+
+  return (
+    <>
+      <MiddleComponent state={state} />
+    </>
+  )
+
+}
+
+const MiddleComponent = (state) => {
+
+  return (
+    <>
+      <BottomComponent state={state} />
+    </>
+  )
+
+}
+
+const BottomComponent = (state) => {
+
+  console.log(state)
+
+  return (
+    <div>
+      {state}
+    </div>
+  )
+
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,11 @@
+import { useContext } from "react"
+import { AppContext } from "../App"
+
+
 export const Home = () => {
+  const {username } = useContext(AppContext)
   return (
 
-    <h1> This is the home page</h1>
+    <h1> This is the home page and user is: {username}</h1>
   )
 }
