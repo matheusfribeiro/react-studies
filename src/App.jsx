7886913ef81be66fc4699fe920f1432d4f0aa896
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-04-16 17:30:49 -0300

    react-router-dom update and new way to fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,49 @@
 import './App.css'
 import Axios from 'axios'
 import { useEffect, useState } from 'react'
-import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom' 
+import {createBrowserRouter, createRoutesFromElements, Route, Link, Outlet, RouterProvider} from 'react-router-dom' 
 import { Home } from './pages/Home'
 import { Menu } from './pages/Menu'
-import { Contact } from './pages/Contact'
+import { Contact, dataLoader } from './pages/Contact'
 import { Navbar } from './NavBar'
 
 
 
 
 function App() {
+
+  const router = createBrowserRouter(
+    createRoutesFromElements(
+      <Route path='/' element={<Root />}>
+        <Route index element={<Home />} />
+        <Route path='/contact' element={<Contact />} loader={dataLoader} />
+        <Route path='/menu' element={<Menu />} />
+      </Route>
+    )
+  )
+
   
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/menu" element={<Menu />}/>
-          <Route path="/contact" element={<Contact />}/>
-          <Route path="*" element={<h1>PAGE NOT FOUND</h1>}/>
-        </Routes>
-      </Router>
+      <h1>Hello react</h1>
+      <RouterProvider router={router} />
     </div>
+  )
+}
+
+const Root = () => {
+  return (
+    <>
+      <div>
+        <Link to="/"> Home </Link>
+        <Link to="/menu"> Menu </Link>
+        <Link to="/contact"> Contact </Link>
+        
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
   )
 }
 
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,27 @@
-export const Contact = () => {
-  return (
+import { useLoaderData, useNavigation } from "react-router-dom"
 
-    <h1> This is the Contact page</h1>
+export const Contact = () => {
+  const dogurl = useLoaderData()
+  const navigation = useNavigation()
+
+  if (navigation.state === "loading") {
+    return <h1>LOADING...</h1>
+  }
+
+  return (
+    <div>
+      
+      <h1> This is the Contact page</h1>
+      <img src={dogurl} alt="" />
+    
+    </div>
+
   )
+}
+
+export const dataLoader = async () => {
+  const res = await fetch("https://random.dog/woof.json")
+  const dog = await res.json()
+
+  return dog.url 
 }
