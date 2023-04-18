84f4c1f39413339ab81980730ce9f7e8f9f3dc50
Matheus F. Ribeiro <matheusfribeiro2112@gmail.com>
2023-04-18 16:44:30 -0300

    custom hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import './App.css'
-import { Form } from './pages/Form'
+import { useToggle } from './useToggle'
 
 
 
 
 function App() {
+  const [isVisible, toggle] = useToggle()
+  const [isVisible2, toggle2] = useToggle()
+
   
   
   return (
     <div className="App">
-      <h1>Hello react</h1>
-      <Form />
+      <button onClick={toggle}>
+        {isVisible ? "Hide" : "Show"}
+      </button>
+      {isVisible && <h1>Hidden text</h1>}
+      
+      <button onClick={toggle2}>
+        {isVisible2 ? "Hide" : "Show"}
+      </button>
+      {isVisible2 && <h1>Hidden text 2</h1>}
       
     </div>
   )
